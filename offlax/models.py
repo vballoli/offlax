b7f9cecd3259eab1df7d6215f8896744116bb624
Vaibhav Balloli <balloli.vb@gmail.com>
2023-01-26 11:52:59 +0000

    Add Actor discrete

--- offlax/models.py
+++ offlax/models.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 from copy import deepcopy
-from typing import List, Tuple
+from typing import List, Tuple, Union
 
 import jax
 import numpy as np
@@ -30,7 +30,7 @@ class Policy(nn.Module):
         return config.save(*args, **kwargs)
 
 
-class Actor(Policy):
+class ActorContinuous(Policy):
     hidden_dim: int
     output_dim: int
 
@@ -45,7 +45,7 @@ class Actor(Policy):
 
         return mu, log_std
 
-    def get_action(self, variables: VariableDict, state: jnp.ndarray, key: jax.random.PRNGKey, deterministic: bool=True, return_log_prob: bool=True) -> jnp.ndarray:
+    def get_action(self, variables: VariableDict, state: jnp.ndarray,  key: jax.random.PRNGKey, deterministic: bool=False, return_log_prob: bool=True) -> jnp.ndarray:
         mu, log_std = self.apply(variables, state)
 
         if deterministic:
@@ -54,7 +54,7 @@ class Actor(Policy):
 
         # normal distribution
         normal_dist = tfd.Normal(mu, jnp.exp(log_std))
-        normal_dist_sample = tfd.Sample(normal_dist)
+        normal_dist_sample = normal_dist.sample(seed=key)
         action = jnp.tanh(normal_dist_sample)
 
         if return_log_prob:
@@ -70,6 +70,40 @@ class Actor(Policy):
 
         return config
 
+
+class ActorDiscrete(Policy):
+    hidden_dim: int
+    output_dim: int
+
+    @nn.compact
+    def __call__(self, x: jnp.ndarray) -> jnp.ndarray:
+        x = nn.relu(nn.Dense(self.hidden_dim, name=f"linear1_{self.__repr__()}")(x))
+        x = nn.relu(nn.Dense(self.hidden_dim, name=f"linear2_{self.__repr__()}")(x))
+        x = nn.Dense(self.output_dim, name=f"linear3_{self.__repr__()}")(x)
+
+        return nn.softmax(x)
+
+    def get_action(self, variables: VariableDict, state: jnp.ndarray, key: jax.random.PRNGKey, deterministic: bool=False, return_log_prob: bool=True) -> Tuple[jnp.ndarray, jnp.ndarray]:
+        action = self.apply(variables, state)
+        categorical_distribution = tfd.Categorical(action)
+
+        action_probability = categorical_distribution.sample(seed=key)
+
+        if deterministic:
+            return action_probability
+
+        if return_log_prob:
+            log_action_probability = jnp.log(jnp.asarray(action_probability == 0.).astype('float') * 1e-8 + action_probability)
+            return action, action_probability, log_action_probability
+
+        return action_probability
+
+    def get_config_dict(self) -> OmegaConf:
+        config = OmegaConf.create({})
+        config['hidden_dim'] = self.hidden_dim
+        config['output_dim'] = self.output_dim
+
+        return config
 
 
 class Critic(Policy):
@@ -91,4 +125,3 @@ class Critic(Policy):
         config['output_dim'] = self.output_dim
 
         return config
-
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,15 +1,16 @@
-from offlax.models import Actor, Critic
+from typing import Tuple
+from offlax.models import ActorDiscrete, ActorContinuous, Critic
 
 import jax
 from jax import numpy as jnp
 from omegaconf import OmegaConf
 
 
-def test_actor():
+def test_actor_continuous():
     rng = jax.random.PRNGKey(0)
     rng, init_rng = jax.random.split(rng)
 
-    actor = Actor(64, 64)
+    actor = ActorContinuous(64, 64)
     actor_variables = actor.init(init_rng, jnp.ones((1, 20)))
 
     state = jnp.ones((1, 20))
@@ -18,6 +19,26 @@ def test_actor():
     a = actor.get_action(actor_variables, state, rng)
     assert isinstance(mu, jnp.ndarray)
     assert isinstance(sigma, jnp.ndarray)
+
+    config_dict = actor.get_config_dict()
+    assert config_dict['hidden_dim'] == 64
+    assert config_dict['output_dim'] == 64
+
+def test_actor_discrete():
+    rng = jax.random.PRNGKey(0)
+    rng, init_rng = jax.random.split(rng)
+
+    actor = ActorDiscrete(64, 64)
+    actor_variables = actor.init(init_rng, jnp.ones((1, 20)))
+
+    state = jnp.ones((1, 20))
+    action = actor.apply(actor_variables, state)
+
+    action = actor.get_action(actor_variables, state, rng, return_log_prob=True)
+    assert isinstance(action, Tuple)
+    assert len(action) == 3
+    # assert isinstance(mu, jnp.ndarray)
+    # assert isinstance(sigma, jnp.ndarray)
 
     config_dict = actor.get_config_dict()
     assert config_dict['hidden_dim'] == 64
